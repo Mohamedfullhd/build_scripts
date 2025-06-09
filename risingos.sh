f1f8fdc814dc5cbf5f8d47ef6eea2c894609bac7
Mohamedfullhd <aaamohamed312@gmail.com>
2025-06-09 17:48:05 +0300

    Update risingos.sh

--- risingos.sh
+++ risingos.sh
@@ -4,27 +4,22 @@
 rm -rf .repo/local_manifests
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
+repo init -u https://github.com/RisingOS-Revived/android.git -b qpr2 --git-lfs
 
 # Clone local_manifests repository
-git clone https://github.com/shravansayz/local_manifests --depth 1 -b rise .repo/local_manifests
+git clone https://github.com/Mohamedfullhd/Build-.git .repo/local_manifests
 
 # Sync the repositories
 /opt/crave/resync.sh
-
-#customs
-rm -rf frameworks/base
-git clone https://github.com/shravansayz/android_frameworks_base_rise.git -b fifteen frameworks/base --depth=1
 
 #Private Keys
 rm -rf vendor/lineage-priv
 git clone https://github.com/shravansayz/private_keys.git -b rise vendor/lineage-priv
 
-export BUILD_USERNAME=shravan
+export BUILD_USERNAME=Mohamed
 export BUILD_HOSTNAME=crave
 
 #build
 source build/envsetup.sh
-riseup RMX1901 user
-m installclean
+riseup gale bp1a || riseup gale userdebug
 rise b
