9b33586b7bcc747151846ec74166d559fa26a823
Mohamedfullhd <aaamohamed312@gmail.com>
2025-06-20 03:42:40 +0300

    Update genesis.sh

--- genesis.sh
+++ genesis.sh
@@ -4,7 +4,7 @@
 rm -rf .repo/local_manifests
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/GenesisOS/manifest.git -b verve --git-lfs
+repo init -u https://github.com/GenesisOS/manifest.git -b verve-qpr2 --git-lfs
 
 # Clone local_manifests repository
 git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b genesis .repo/local_manifests
